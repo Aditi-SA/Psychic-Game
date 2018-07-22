593e53ff8333d4c7c5bac12e21e25174fce587ca
FionaViola <aditi.nadig10@gmail.com>
2018-07-21 21:42:08 -0400

    Final Psychic-Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,9 @@
     // Creating variables to hold the number of wins, losses and attmpts made to guess the correct alphabet. They start at 0.
     var wins = 0;
     var losses = 0;    
-    var attempts=15;
+    var attempts=5;
     var LetterToGuess= "";
+    var Counter = 0;
 
     var TriedVar=[];
     UserSelectedDiv = document.getElementById("UserSelected");
@@ -13,55 +14,72 @@
     //Function 
     var RandomVar = function(){
       // Randomly chooses a choice from the options array of Letters. This is the Computer's guess.
-      LetterToGuess = Letters[Math.floor(Math.random() * Letters.length)];
-      console.log(LetterToGuess);
+      LetterToGuess = Letters[Math.floor(Math.random() * Letters.length)].toLowerCase();
+      //console.log("LetterToGuess: " + LetterToGuess);
     }
-    //CAll function to generate a random character from the array Letters[].
-    RandomVar();
-
+    
     // This function is run whenever the user presses a key.
-    document.onkeyup = function(event) {
-      
-      // Determines which key was pressed.
-      var userGuess = event.key;    
-      
-      //// Randomly chooses a choice from the options array of Letters. This is the Computer's guess.
-      //var LetterToGuess = Letters[Math.floor(Math.random() * Letters.length)];
+    function Psychic(){
+        //CAll function to generate a random character from the array Letters[].
+        RandomVar();
 
-      // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-     
-        if (userGuess === LetterToGuess){
-          wins++;
-          //document.write("You Win!!!");
-          RandomVar();                   
-          // function to choose LetterToGuess
+        //Reset No of guesses
+        attempts=5;
+
+        //Clear the array of alphabets Guessed So Far
+        while (TriedVar.length) {
+            TriedVar.pop();
         }
-        else {           
-          attempts--; 
-          if (attempts === 0){
-            document.write("You Lost!!!");
-            RandomVar();
-            losses++; 
-          }
-        }
-        TriedVar.push(userGuess);
-        //document.getElementById("UserSelected").innerHTML = "Letters already selected: " + TriedVar + " ";
-              
-        // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
-        var html =
-          "<p>You chose: " + userGuess + "</p>" +
-          "<p>No of attempts left: " + attempts + "</p>" ;
-                  
-        var tally =    
-        "<p>wins: " + wins + "</p>" +
-        "<p>losses: " + losses + "</p>" ;
 
-        // Set the inner HTML contents of the #game div to our html string
+        document.onkeyup = function(event) {
+                        
+            //Continue with the game only if an alphabet is keyed in else send an alert message!
+            if (event.keyCode >= 65 && event.keyCode <= 90) {
+                
+                // Determines which key was pressed. Convert the key to lower case
+                var userGuess = event.key.toLowerCase();
+                                    
+                // This logic determines the outcome of the game (win/loss), and increments the appropriate count
+                TriedVar.push(userGuess);   
+                if (userGuess === LetterToGuess){
+                    wins++;                    
+                    // End this round and run the game again
+                    Psychic();                    
+                }
+                else {           
+                    attempts--; 
+                    if (attempts == 0){                         
+                        losses++;                      
+                        // End this round and run the game again
+                        Psychic();                        
+                    }
+                }
+                
+                // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses.
+                var html =
+                //"<p>You chose: " + userGuess + "</p>" +
+                "<p>No of Guesses left: " + attempts + "</p>" ;
+                //"<p>The computer chose: " + LetterToGuess + "</p>" +
+                
+                var tally =    
+                "<p>wins: " + wins + "</p>" +
+                "<p>losses: " + losses + "</p>" ;
         
-        document.querySelector("#CurrentGameStatus").innerHTML = tally;        
+                // Set the inner HTML contents of the #game div to our tally string                
+                document.querySelector("#CurrentGameStatus").innerHTML = tally;
+                
+                // Set the inner HTML contents of the #scores div to our html string
+                document.querySelector("#scores").innerHTML = "Current Game Status: " + html;
+                
+                // Set the inner HTML contents of the #UserSelected div to our html array TriedVar
+                document.getElementById("UserSelected").innerHTML = "Your Guesses So Far: " + TriedVar + " ";
+            
+            } 
+            else{
+                alert("Incorrect Input: Not an alphabet, Select again!");
+            }
+        };        
+    }
 
-        document.querySelector("#scores").innerHTML = "Current Game Status: " + html;
-        
-        document.getElementById("UserSelected").innerHTML = "Letters already selected: " + TriedVar + " ";
-        
-    };
+
+    Psychic();
